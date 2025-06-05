7ff65f03cf52ff09f46b7c75a41cd840e4d4b140
Mike O'Brien <mikeob9@gmail.com>
2025-06-05 12:48:14 -0400

    call stats package

--- tests/testthat/test-COA_Tag_Integrated.R
+++ tests/testthat/test-COA_Tag_Integrated.R
@@ -10,12 +10,12 @@ testloc # Test tag location
 # Define state-space of point process - 'buffer' adds a fixed buffer to the outer extent of the recs
 buffer <- 1
 xlim <- c(min(rlocs$east - buffer), max(rlocs$east + buffer))
-ylim <- c(min(rlocs$north - buffer),max(rlocs$north + buffer))
+ylim <- c(min(rlocs$north - buffer), max(rlocs$north + buffer))
 # Set up a blank vector for storage
 D <- NULL
 # Loop over each hour
-for( i in 1:nrow(testdat) ){
-  D[i] <- distf(testloc[,c('east', 'north')], testdat[i, c('east', 'north')] )
+for (i in 1:nrow(testdat)) {
+  D[i] <- distf(testloc[, c('east', 'north')], testdat[i, c('east', 'north')])
 }
 
 testdat$D <- unlist(D) # Assign to testdat data frame
@@ -28,27 +28,29 @@ m1$dist <- round(m1$D * 1000)
 m1$label <- paste(m1$dist, "m", "(", m1$Station, ")")
 
 
-fishdat <- fishdat[fishdat$hour < 11,]
-testdat <- testdat[testdat$hour < 11,]
+fishdat <- fishdat[fishdat$hour < 11, ]
+testdat <- testdat[testdat$hour < 11, ]
 
 # Create a copy of the receiver locations for tallying
 rs <- rlocs
 # Add column for each time interval to indicate whether receiver was operational or not
-rs[, c(4:(max(fishdat$hour) + 3 ) )] <- 1
-rs <- dplyr::rename( rs,
-                     `1`="...4",
-                     `2`="...5",
-                     `3`="...6",
-                     `4`="...7",
-                     `5`="...8",
-                     `6`="...9",
-                     `7`="...10",
-                     `8`="...11",
-                     `9`="...12",
-                     `10`="...13")
+rs[, c(4:(max(fishdat$hour) + 3))] <- 1
+rs <- dplyr::rename(
+  rs,
+  `1` = "...4",
+  `2` = "...5",
+  `3` = "...6",
+  `4` = "...7",
+  `5` = "...8",
+  `6` = "...9",
+  `7` = "...10",
+  `8` = "...11",
+  `9` = "...12",
+  `10` = "...13"
+)
 
 # Create vector of the number of sampling occasions for each receiver
-tsteps <- apply(rs[,4:ncol(rs)],1,sum)
+tsteps <- apply(rs[, 4:ncol(rs)], 1, sum)
 
 
 ## Starting with the test tag data
@@ -56,26 +58,33 @@ testdat$rec <- as.numeric(substr(testdat$Station, 3, 4))
 testdat$count <- 1 # Add a column that indicates each record corresponds to 1 detection
 
 # Aggregate the number of detections for each individual at each receiver in each time step
-test.agg <- aggregate(count ~ Transmitter + rec + east + north + hour, testdat, sum)
+test.agg <- stats::aggregate(
+  count ~ Transmitter + rec + east + north + hour,
+  testdat,
+  sum
+)
 # Create a numeric identifier for each transmitter
 test.agg$tag <- as.numeric(as.factor(test.agg$Transmitter))
 # Rename hour to time for consistency when plotting below
 test.agg$time <- test.agg$hour
 
 # Specify quantities for indexing
-ntest <- length( unique(test.agg$Transmitter) ) # number of individual tags (here just the one test tag)
+ntest <- length(unique(test.agg$Transmitter)) # number of individual tags (here just the one test tag)
 nrec <- nrow(rlocs) # number of receivers
 tsteps <- max(test.agg$hour)
 
 # This chunk saves the total number of encounters of each individual (rows) in each trap (cols) at each sampling occasion (array elements)
-testY <- array( NA, dim = c( ntest, nrec, tsteps ) )
-for( t in 1:max(tsteps)){
-  for ( i in 1:nrow(testY) ){
-    h1 <- test.agg[test.agg$tag==i,]
-    for( j in 1:nrow(rlocs) ){
+testY <- array(NA, dim = c(ntest, nrec, tsteps))
+for (t in 1:max(tsteps)) {
+  for (i in 1:nrow(testY)) {
+    h1 <- test.agg[test.agg$tag == i, ]
+    for (j in 1:nrow(rlocs)) {
       # If there are no detections at that receiver in that time period, set to 0; otherwise set to the number of detections
-      testY[i,j,t]=ifelse(identical(h1[h1$hour==t & h1$rec==j,6],
-                                    numeric(0)),0, h1[h1$hour==t & h1$rec==j,6])
+      testY[i, j, t] = ifelse(
+        identical(h1[h1$hour == t & h1$rec == j, 6], numeric(0)),
+        0,
+        h1[h1$hour == t & h1$rec == j, 6]
+      )
     }
   }
 }
@@ -85,23 +94,31 @@ fishdat$rec <- as.numeric(substr(fishdat$Station, 3, 4))
 fishdat$count <- 1 # Add a column that indicates each record corresponds to 1 detection
 
 # Aggregate the number of detections for each individual at each receiver in each time step
-fish.agg <- aggregate(count ~ Transmitter + rec + east + north + hour, fishdat, sum)
+fish.agg <- stats::aggregate(
+  count ~ Transmitter + rec + east + north + hour,
+  fishdat,
+  sum
+)
 # Create a numeric identifier for each transmitter
 fish.agg$tag <- as.numeric(as.factor(fish.agg$Transmitter))
 # Rename hour to time for consistency when plotting below
 fish.agg$time <- fish.agg$hour
 
 # Specify quantities for indexing
-nind <- length( unique(fish.agg$Transmitter) ) # number of individual tags (here just the one test tag)
+nind <- length(unique(fish.agg$Transmitter)) # number of individual tags (here just the one test tag)
 
 # This chunk saves the total number of encounters of each individual (rows) in each trap (cols) at each sampling occasion (array elements)
-Y=array( NA, dim=c( nind, nrec, tsteps ) )
-for( t in 1:max(tsteps) ){
-  for ( i in 1:nrow(Y) ){
-    h1 <- fish.agg[fish.agg$tag==i,]
-    for( j in 1:nrow(rlocs) ){
+Y = array(NA, dim = c(nind, nrec, tsteps))
+for (t in 1:max(tsteps)) {
+  for (i in 1:nrow(Y)) {
+    h1 <- fish.agg[fish.agg$tag == i, ]
+    for (j in 1:nrow(rlocs)) {
       # If there are no detections at that receiver in that time period, set to 0; otherwise set to the number of detections
-      Y[i,j,t]=ifelse(identical(h1[h1$hour==t & h1$rec==j,6], numeric(0)),0, h1[h1$hour==t & h1$rec==j,6])
+      Y[i, j, t] = ifelse(
+        identical(h1[h1$hour == t & h1$rec == j, 6], numeric(0)),
+        0,
+        h1[h1$hour == t & h1$rec == j, 6]
+      )
     }
   }
 }
@@ -110,30 +127,30 @@ for( t in 1:max(tsteps) ){
 ## Specify the number of sentinal tags (this step is necessary because of issues that arise with Stan indexing if you have only 1 test tag)
 nsentinal <- 1
 
-fit_tag <- COA_TagInt(nind=nind, # number of individuals
-                      nrec=nrec, # number of receivers
-                      ntime=tsteps, # number of time steps
-                      ntest=nsentinal, # number of test tags
-                      ntrans=30, # number of expected transmissions per tag per time interval
-                      y=Y,   # array of detections from tagged fish
-                      test=testY, # array of detections from test tags
-                      recX=as.vector(rlocs$east),  # E-W receiver coordinates
-                      recY=as.vector(rlocs$north), # N-S receiver coordinates
-                      xlim=xlim, # E-W boundary of spatial extent (receiver array + buffer)
-                      ylim=ylim, # N-S boundary of spatial extent (receiver array + buffer)
-                      testX=array(testloc$east,dim=c(nsentinal)),
-                      testY=array(testloc$north,dim=c(nsentinal)),
-                      iter = 4000,
-                      warmup = 2000,
-                      control = list(adapt_delta = 0.95),
-                      chain = 2)
+fit_tag <- COA_TagInt(
+  nind = nind, # number of individuals
+  nrec = nrec, # number of receivers
+  ntime = tsteps, # number of time steps
+  ntest = nsentinal, # number of test tags
+  ntrans = 30, # number of expected transmissions per tag per time interval
+  y = Y, # array of detections from tagged fish
+  test = testY, # array of detections from test tags
+  recX = as.vector(rlocs$east), # E-W receiver coordinates
+  recY = as.vector(rlocs$north), # N-S receiver coordinates
+  xlim = xlim, # E-W boundary of spatial extent (receiver array + buffer)
+  ylim = ylim, # N-S boundary of spatial extent (receiver array + buffer)
+  testX = array(testloc$east, dim = c(nsentinal)),
+  testY = array(testloc$north, dim = c(nsentinal)),
+  iter = 4000,
+  warmup = 2000,
+  control = list(adapt_delta = 0.95),
+  chain = 2
+)
 
 #
-rstan::traceplot(fit_tag$model, pars = c("alpha1",
-                                     "sigma", "lp__"))
+rstan::traceplot(fit_tag$model, pars = c("alpha1", "sigma", "lp__"))
 fit_tag$coas
 test_that("test COA_TagInt model results to make sure its consisitent", {
-
   mean_p0 <- fit_tag$summary[1]
   expected_mean_p0 <- 0.486
   expect_equal(mean_p0, expected_mean_p0, tolerance = 0.05)
