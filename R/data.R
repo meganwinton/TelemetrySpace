4106c15648776ec8edacf22c81dd40f11ca53702
benjaminhlina <benjamin.hlina@gmail.com>
2025-06-18 11:05:17 -0400

    Added roxygen documentation in Data.R for Y, example_extent, and model_param_ex

--- R/data.R
+++ R/data.R
@@ -32,3 +32,34 @@
 #'    \code{east} East-West coordinate, \code{north} North-South coordinate, \code{hour} Hour of monitoring.
 #'   
 "fishdat"
+
+#' Example model parameters
+#'
+#' Example model parameters for `COA_standard()`
+#'
+#' @format A data frame with four variables and one row:
+#' `nind` which is the number of individuals, `nrec` is the number of receivers
+#' in the array, `tsteps` is the number of time steps used in the example,
+#' and `ntrans` is the number of expected transmissions within a time step.
+#'
+"model_param_ex"
+
+#' Example array extent
+#'
+#' Example array extent used in each model
+#'
+#' @format A data frame with two variables and two row: `ylim` is the minimum
+#' and maximum extent on the y-axis (i.e., latitudinal) for the array and
+#' `xlim` is the minimum and maximum extent on the x-axis (i.e., longitudinal)
+#' for the array.
+#'
+"example_extent"
+
+#' Counts of detection per time steps
+#'
+#' Array of counts of detection per time step per receivers
+#'
+#' @format An array with demensions of 1 by 10 (number of time steps) by 30 number
+#' of receivers.
+#'
+"Y"
