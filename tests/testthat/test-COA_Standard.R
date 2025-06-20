5540dce097d609fe2397efd7ff01bfdbf9f530f9
benjaminhlina <benjamin.hlina@gmail.com>
2025-06-20 12:56:13 -0400

    Add test for y

--- tests/testthat/test-COA_Standard.R
+++ tests/testthat/test-COA_Standard.R
@@ -170,8 +170,30 @@ test_that("test ntime to see if it errors", {
 }
 )
 # ---- check y -----
+test_that("test y to see if errors", {
 
+  expect_error(
+    COA_Standard(
+      nind = model_param_ex$nind,  # number of individuals
+      nrec = model_param_ex$nrec, # number of receivers
+      ntime = model_param_ex$tsteps, # number of time steps
+      ntrans = model_param_ex$ntrans,
+      # number of expected transmissions per tag per time interval
+      y = c(1, 2, 3), # array of detections
+      recX = rlocs$east, # E-W receiver coordinates
+      recY = rlocs$north, # N-S receiver coordinates
+      xlim = example_extent$xlim, # E-W boundary of spatial extent (receiver array + buffer)
+      ylim = example_extent$ylim, # N-S boundary of spatial extent (receiver array + buffer)
+      chains = 2,
+      warmup = 1000,
+      iter = 2000,
+      control = list(adapt_delta = 0.95)
+    ),
+    regexp = "'y' must be a 3-dimensional numeric array."
+  )
 
+}
+)
 
 
 
