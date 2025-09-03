804be41b4b2f4c238d4fb68d6bf8afbe4a962592
benjaminhlina <benjamin.hlina@gmail.com>
2025-09-03 15:59:29 -0400

    Add in global variables for the pkg in zzz.

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,23 @@
   modules <- paste0("stan_fit4", names(stanmodels), "_mod")
   for (m in modules) loadModule(m, what = TRUE)
 }
+
+# ---- global variables ------
+# add variables going forward in alphabetical order
+utils::globalVariables(
+  c(
+    "alpha0",
+    "alpha1",
+    "nind",
+    "nrec",
+    "ntest",
+    "ntime",
+    "ntrans",
+    "recX",
+    "recY",
+    "sx",
+    "sy",
+    "testX",
+    "testY"
+  )
+)
